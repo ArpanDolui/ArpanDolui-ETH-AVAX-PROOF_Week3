cf203254e2eebbfd22e30009c4e785492f7a16e5
ArpanDolui <107446003+ArpanDolui@users.noreply.github.com>
2024-06-19 02:22:19 +0530

    Update Week3_Modiul3.sol

--- Week3_Modiul3.sol
+++ Week3_Modiul3.sol
@@ -1,1 +1,31 @@
+pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
 
+contract Token {
+    address private owner;
+    mapping (address => uint256) public balance;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier only_Owner() {
+        require(msg.sender == owner, "Only the contract owner can mint tokens");
+        _;
+    }
+
+    function mint(address recipient, uint256 amount) public only_Owner {
+        balance[recipient] += amount;
+    }
+
+    function transfer(address recipient, uint256 amount) public {
+        require(balance[msg.sender] >= amount, "Insufficient balance");
+        balance[msg.sender] -= amount;
+        balance[recipient] += amount;
+    }
+
+    function burn(uint256 amount) public {
+        require(balance[msg.sender] >= amount, "Insufficient balance");
+        balance[msg.sender] -= amount;
+    }
+}
